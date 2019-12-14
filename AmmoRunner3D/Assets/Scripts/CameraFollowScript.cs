1e1126d98f4ff915bd68e20ae974c1c11f8bb313
HornRedFire <58868056+HornRedFire@users.noreply.github.com>
2019-12-14 14:15:16 -0500

    Added Camera Follow
    Camera now follows. Rejoice.

---
+++ AmmoRunner3D/Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;     //the offset distance between the player and the camera
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //calculate the value by getting the distance between the player position and camera position
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
